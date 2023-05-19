a4896dc0ee582a5e48323a42940675fe1f8e657c
roosman <70866116+os-rooney@users.noreply.github.com>
2023-05-19 19:27:22 +0200

    Update CinemaRoomManager.kt
    Step 5/5

--- medium_level/cinema_room_manager/CinemaRoomManager.kt
+++ medium_level/cinema_room_manager/CinemaRoomManager.kt
@@ -1,78 +1,85 @@
+import java.util.*
+
 fun main() {
-  // Read the number of rows from user input
-  print("Enter the number of rows: ")
-  val numRows = readLine()!!.toInt()
+    val scanner = Scanner(System.`in`)
+    println("Enter the number of rows:")
+    val rows = scanner.nextInt()
+    println("Enter the number of seats in each row:")
+    val seats = scanner.nextInt()
 
-  // Read the number of seats per row from user input
-  print("Enter the number of seats on each row: ")
-  val numSeatsPerRow = readLine()!!.toInt()
+    val cinema = MutableList(rows) { MutableList(seats) { true } }
+    var totalIncome = 0
+    var currentIncome = 0
+    var boughtTickets = 0
 
-  // Initialize a mutable list to represent the cinema seats
-  val cinema = MutableList(numRows) { MutableList(numSeatsPerRow) { 'S' } }
-
-  // Loop to display menu and handle user input
-  while(true) {
-    println("\n1. Show the seats")
-    println("2. Buy a ticket")
-    println("0. Exit")
-
-    val choice = readLine()!!.toInt()
-
-    when(choice) {
-      1 -> showSeats(cinema)
-      2 -> buyTicket(cinema)
-      3 -> break
-      else -> println("Invalid choice. Please try again.")
+    for (i in 0 until rows) {
+        for (j in 0 until seats) {
+            totalIncome += if (rows * seats > 60 && i >= rows / 2) 8 else 10
+        }
     }
-  }
+
+    while (true) {
+        println("\n1. Show the seats\n2. Buy a ticket\n3. Statistics\n0. Exit")
+        when (scanner.nextInt()) {
+            1 -> showSeats(cinema)
+            2 -> {
+                val ticketPrice = buyTicket(cinema)
+                if (ticketPrice != -1) {
+                    boughtTickets++
+                    currentIncome += ticketPrice
+                }
+            }
+            3 -> showStatistics(boughtTickets, currentIncome, totalIncome, rows * seats)
+            0 -> return
+        }
+    }
 }
 
-// Function to display the current seating arrangement
-fun showSeats(cinema: MutableList<MutableList<Char>>) {
-  println("Cinema: ")
-  print("  ")
-  for (i in 1..cinema[0].size) {
-    print("$i ")
-  }
-  println()
-  for(i in cinema.indices) {
-    print("${i + 1}")
-    for(j in cinema[i].indices) {
-      print("${cinema[i][j]}")
+fun showSeats(cinema: MutableList<MutableList<Boolean>>) {
+    println("\nCinema:")
+    print("  ")
+    for (i in cinema[0].indices) {
+        print("${i + 1} ")
     }
     println()
-  }
-}
-
-// Function to buy a ticket and update the seating arrangement
-fun buyTicket(cinema: MutableList<MutableList<Char>>) {
-  print("Enter a row number: ")
-  val row = readLIne()!!.toInt() - 1
-  print("Enter a seat number in that row: ")
-  val seat = readLine()!!.toInt() - 1
-
-  // check if row and seat are within valid range and is free
-  if (row in 0 until cinema.size && seat in 0 until cinema[row].size && cinema[row][seat] == 'S') {
-    if(cinema[row][seat] == 'B'){
-      println("Error! This seat is already take.")
-    } else {
-      cinema[row][seat] = 'B' // Update seat status to 'B' for taken
-      val ticketPric = getTicketPrice(cinema.size, cinema[0].size, row)
-      println("Ticket price: $$ticketPrice")
+    for (i in cinema.indices) {
+        print("${i + 1} ")
+        for (j in cinema[i].indices) {
+            print(if (cinema[i][j]) "S " else "B ")
+        }
+        println()
     }
-  } else {
-    println("Error! Invalid seat selection")
-  }
 }
 
-// Calculate ticket price
-fun getTicketPrice(numRows: Int, numSeatsPerRow: Int, row: Int) : Int {
-  val totalSeats =  numRows * numSeatsPerRow
-  val frontHalfRows = numRows / 2
-  val ticketPrice = if (totalSeats <= 60 || row < frontHalfRows){
-    10
-  } else {
-    8
-  }
-  return ticketPrice
+fun buyTicket(cinema: MutableList<MutableList<Boolean>>): Int {
+    val scanner = Scanner(System.`in`)
+    while (true) {
+        println("\nEnter a row number:")
+        val row = scanner.nextInt() - 1
+        println("Enter a seat number in that row:")
+        val seat = scanner.nextInt() - 1
+
+        if (row !in cinema.indices || seat !in cinema[row].indices) {
+            println("\nWrong input!")
+            continue
+        }
+
+        if (!cinema[row][seat]) {
+            println("\nThat ticket has already been purchased!")
+            continue
+        }
+
+        cinema[row][seat] = false
+        val ticketPrice = if (cinema.size * cinema[0].size > 60 && row >= cinema.size / 2) 8 else 10
+        println("\nTicket price: $$ticketPrice")
+        return ticketPrice
+    }
+}
+
+fun showStatistics(boughtTickets: Int, currentIncome: Int, totalIncome: Int, totalSeats: Int) {
+    val percentage = "%.2f".format(boughtTickets.toDouble() / totalSeats * 100)
+    println("\nNumber of purchased tickets: $boughtTickets")
+    println("Percentage: $percentage%")
+    println("Current income: $$currentIncome")
+    println("Total income: $$totalIncome")
 }
