3c6b697a2cc9025a7db169c224b56f05bb51b883
roosman <dev@ronios.de>
2023-05-29 12:52:37 +0200

    upload coffe machine solution stage 6/6

--- easy_level/Coffee_Machine/CoffeeMachine.kt
+++ easy_level/Coffee_Machine/CoffeeMachine.kt
@@ -1,7 +1,7 @@
 import java.util.Scanner
 
-class CoffeeMachine(var water: Int, var milk: Int, var coffeeBeans: Int, var cups: Int, var money: Int) {
-    val scanner = Scanner(System.`in`)
+class CoffeeMachine(private var water: Int, private var milk: Int, private var coffeeBeans: Int, private var cups: Int, private var money: Int) {
+    private var state = "choosing action"
 
     fun displayState() {
         println("The coffee machine has:")
@@ -9,10 +9,10 @@ class CoffeeMachine(var water: Int, var milk: Int, var coffeeBeans: Int, var cup
         println("$milk ml of milk")
         println("$coffeeBeans g of coffee beans")
         println("$cups disposable cups")
-        println("$money of money\n")
+        println("$$money of money\n")
     }
 
-    fun canMakeCoffee(waterNeeded: Int, milkNeeded: Int, coffeeBeansNeeded: Int): Boolean {
+    private fun canMakeCoffee(waterNeeded: Int, milkNeeded: Int, coffeeBeansNeeded: Int): Boolean {
         if (water < waterNeeded) {
             println("Sorry, not enough water!")
             return false
@@ -33,77 +33,100 @@ class CoffeeMachine(var water: Int, var milk: Int, var coffeeBeans: Int, var cup
         return true
     }
 
-    fun buy() {
-        while (true) {
-            println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
-            val choice = scanner.nextLine()
-            when (choice) {
-                "1" -> {
-                    if (canMakeCoffee(250, 0, 16)) {
-                        water -= 250
-                        coffeeBeans -= 16
-                        money += 4
-                        cups--
+    fun process(input: String) {
+        when (state) {
+            "choosing action" -> {
+                when (input) {
+                    "buy" -> {
+                        state = "choosing coffee"
+                        println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
                     }
-                    return
-                }
-                "2" -> {
-                    if (canMakeCoffee(350, 75, 20)) {
-                        water -= 350
-                        milk -= 75
-                        coffeeBeans -= 20
-                        money += 7
-                        cups--
+                    "fill" -> {
+                        state = "filling water"
+                        println("Write how many ml of water you want to add: ")
                     }
-                    return
-                }
-                "3" -> {
-                    if (canMakeCoffee(200, 100, 12)) {
-                        water -= 200
-                        milk -= 100
-                        coffeeBeans -= 12
-                        money += 6
-                        cups--
+                    "take" -> {
+                        println("I gave you $$money")
+                        money = 0
+                        state = "choosing action"
                     }
-                    return
+                    "remaining" -> {
+                        displayState()
+                        state = "choosing action"
+                    }
+                    "exit" -> state = "exit"
                 }
-                "back" -> return
+            }
+            "choosing coffee" -> {
+                when (input) {
+                    "1" -> {
+                        if (canMakeCoffee(250, 0, 16)) {
+                            water -= 250
+                            coffeeBeans -= 16
+                            money += 4
+                            cups--
+                        }
+                        state = "choosing action"
+                    }
+                    "2" -> {
+                        if (canMakeCoffee(350, 75, 20)) {
+                            water -= 350
+                            milk -= 75
+                            coffeeBeans -= 20
+                            money += 7
+                            cups--
+                        }
+                        state = "choosing action"
+                    }
+                    "3" -> {
+                        if (canMakeCoffee(200, 100, 12)) {
+                            water -= 200
+                            milk -= 100
+                            coffeeBeans -= 12
+                            money += 6
+                            cups--
+                        }
+                        state = "choosing action"
+                    }
+                    "back" -> state = "choosing action"
+                }
+            }
+            "filling water" -> {
+                water += input.toInt()
+                println("Write how many ml of milk you want to add: ")
+                state = "filling milk"
+            }
+            "filling milk" -> {
+                milk += input.toInt()
+                println("Write how many grams of coffee beans you want to add: ")
+                state = "filling beans"
+            }
+            "filling beans" -> {
+                coffeeBeans += input.toInt()
+                println("Write how many disposable cups you want to add: ")
+                state = "filling cups"
+            }
+            "filling cups" -> {
+                cups += input.toInt()
+                state = "choosing action"
             }
         }
     }
 
-    fun fill() {
-        println("Write how many ml of water you want to add: ")
-        water += scanner.nextInt()
-        scanner.nextLine()  // to consume the newline left-over
-        println("Write how many ml of milk you want to add: ")
-        milk += scanner.nextInt()
-        scanner.nextLine()
-        println("Write how many grams of coffee beans you want to add: ")
-        coffeeBeans += scanner.nextInt()
-        scanner.nextLine()
-        println("Write how many disposable cups you want to add: ")
-        cups += scanner.nextInt()
-        scanner.nextLine()
-    }
-
-    fun take() {
-        println("I gave you $$money")
-        money = 0
+    fun getState(): String {
+        return state
     }
 }
 
 fun main() {
+    val scanner = Scanner(System.`in`)
     val coffeeMachine = CoffeeMachine(400, 540, 120, 9, 550)
 
     while (true) {
-        println("\nWrite action (buy, fill, take, remaining, exit): ")
-        when (Scanner(System.`in`).nextLine()) {
-            "buy" -> coffeeMachine.buy()
-            "fill" -> coffeeMachine.fill()
-            "take" -> coffeeMachine.take()
-            "remaining" -> coffeeMachine.displayState()
-            "exit" -> return
+        val input = scanner.nextLine()
+        coffeeMachine.process(input)
+        if (coffeeMachine.getState() == "exit") {
+            break
         }
     }
 }
