0166dca3255d291f021a7cd1cfd139aa63ee5995
roosman <dev@ronios.de>
2023-05-29 18:41:16 +0200

    upload minesweeper solution - Look around you - stage 3/6

--- medium_level/minesweeper/minesweeper.kt
+++ medium_level/minesweeper/minesweeper.kt
@@ -4,10 +4,11 @@ fun main() {
     val size = 9
 
     println("How many mines do you want on the field?")
-    val mineCount = readln().toIntOrNull() ?: 0
+    val mineCount = readLine()?.toIntOrNull() ?: 0
 
     val minefield = generateMinefield(size, mineCount)
-    printMinefield(minefield)
+    val fieldWithHints = addHintsToMinefield(minefield)
+    printMinefield(fieldWithHints)
 }
 
 fun generateMinefield(size: Int, mineCount: Int): MutableList<MutableList<Char>> {
@@ -25,6 +26,38 @@ fun generateMinefield(size: Int, mineCount: Int): MutableList<MutableList<Char>>
     }
 
     return minefield
+}
+
+fun addHintsToMinefield(minefield: MutableList<MutableList<Char>>): MutableList<MutableList<Char>> {
+    val size = minefield.size
+
+    for (row in 0 until size) {
+        for (col in 0 until size) {
+            if (minefield[row][col] == '.') {
+                val hintCount = countMinesAroundCell(minefield, row, col)
+                if (hintCount > 0) {
+                    minefield[row][col] = hintCount.toString().first()
+                }
+            }
+        }
+    }
+
+    return minefield
+}
+
+fun countMinesAroundCell(minefield: MutableList<MutableList<Char>>, row: Int, col: Int): Int {
+    val size = minefield.size
+    var mineCount = 0
+
+    for (i in row - 1..row + 1) {
+        for (j in col - 1..col + 1) {
+            if (i in 0 until size && j in 0 until size && minefield[i][j] == 'X') {
+                mineCount++
+            }
+        }
+    }
+
+    return mineCount
 }
 
 fun printMinefield(minefield: MutableList<MutableList<Char>>) {
